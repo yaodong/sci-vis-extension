18f5a8619a303f7d668bbc9f49bd568c168ce8ef
Yaodong Zhao <y@yaodong.org>
2017-02-14 15:39:33 -0700

    Set preview image size and disable auto refresh

--- web/app/routes/jobs/index.js
+++ web/app/routes/jobs/index.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let interval = 1000 * 30;
-    Ember.run.later(this, () => {
-      this.model().then(function(json) {
-        this.controller.set('model', json);
-      }.bind(this));
-    }, interval);
+    //let interval = 1000 * 30;
+    //Ember.run.later(this, () => {
+    //  this.model().then(function(json) {
+    //    this.controller.set('model', json);
+    //  }.bind(this));
+    //}, interval);
 
     return this.get('store').findAll('job', { reload: true }).then((jobs) => {
       return jobs.sortBy('-id');
