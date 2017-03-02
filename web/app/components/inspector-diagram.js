d5c567fca46539ab113ce401c973c3ebbea6ce0a
Yaodong Zhao <y@yaodong.org>
2017-03-02 12:07:06 -0700

    Update preview image urls

--- web/app/components/inspector-diagram.js
+++ web/app/components/inspector-diagram.js
@@ -3,15 +3,15 @@ import Config from 'webapp/config/environment';
 
 export default Ember.Component.extend({
 
-  prefix: '',
+  type: '',
 
   baseUrl: Ember.computed("model", function() {
-    return Config.APP.API_HOST + "/static/jobs/"+ this.get("model.ticket") + "/images";
+    return Config.APP.API_HOST + "/static/jobs/"+ this.get("model.ticket");
   }),
 
-  url: Ember.computed("baseUrl", "zx", "zy", "prefix", function() {
+  url: Ember.computed("baseUrl", "zx", "zy", "type", function() {
     let baseUrl = this.get('baseUrl');
-    return baseUrl + '/' + this.get('prefix') + this.get('zx') + '__' + this.get('zy') + '.png';
+    return baseUrl + '/rotated_' + this.get('zx') + '__' + this.get('zy') + '-' + this.get("type") + '.png';
   })
 
 });
