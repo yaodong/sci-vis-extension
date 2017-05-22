18fc26d80ded428435528c6f4e54c00fbbb926d5
Yaodong Zhao <zhao@yaodong.org>
2017-05-22 17:20:02 -0600

    Clean codes

--- api/jobs/tasks/graph.py
+++ api/jobs/tasks/graph.py
@@ -1,5 +1,4 @@
 from celery import shared_task
-from jobs.utils.dipha import *
 from jobs.utils.graph import *
 from jobs.utils.job import job_get
 from jobs.utils.sphere import sphere_random_directions
@@ -7,6 +6,7 @@ from jobs.utils.point_cloud import project_point_cloud
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import animation
 import numpy as np
+import logging
 
 PROGRESS_PREVIEW_READY = 5
 PROGRESS_ALMOST_DONE = 95
@@ -20,6 +20,8 @@ FILE_TYPE_DELIMITERS = {
 @shared_task()
 def compute_graph(job_id, data_file):
     job = job_get(job_id)
+
+    logging.info('start job %i' % job_id)
 
     work_dir = path.dirname(data_file)
     distance_matrix_file = path.join(work_dir, 'base_distance_matrix')
